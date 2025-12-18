7cd7cc42061436518677be0f2bf5e0c4acb86cd3
clemsRp <clement.rappo@edmu.fr>
2025-12-18 22:19:52 +0100

    Settings all files to the flake8 standard

--- ex5/ft_count_harvest_iterative.py
+++ ex5/ft_count_harvest_iterative.py
@@ -1,17 +1,6 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_count_harvest_iterative.py                      :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: crappo <crappo@student.42.fr>              +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2025/12/09 13:08:57 by crappo            #+#    #+#              #
-#    Updated: 2025/12/09 13:11:45 by crappo           ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
 
 def ft_count_harvest_iterative():
-	days = int(input("Days until harvest: "))
-	for k in range(days):
-		print("Day", k + 1)
-	print("Harvest time!")
+    days = int(input("Days until harvest: "))
+    for k in range(days):
+        print("Day", k + 1)
+    print("Harvest time!")
